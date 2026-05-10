f6af4d369c4956669bdc272e33d841e1a986b7a1
ClueHouse <251077386+ClueHouse@users.noreply.github.com>
2026-05-11 10:41:45 +1200

    Update renderBaseStationStandard.js

--- public/modules/renderBaseStationStandard.js
+++ public/modules/renderBaseStationStandard.js
@@ -104,7 +104,7 @@ export function renderBaseStationStandard(app, data = {}, navigate) {
 .ww-title-meta{
   position:absolute;
   z-index:18;
-  left:15%;
+  left:16%;
   top:16%;
   width:30%;
   color:rgba(255,244,224,0.88);
