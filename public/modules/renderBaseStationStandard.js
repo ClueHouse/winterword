edef5edb7b3d8eaa3c2b88cbb19308b9f30ef013
ClueHouse <251077386+ClueHouse@users.noreply.github.com>
2026-05-18 12:25:36 +1200

    Update renderBaseStationStandard.js

--- public/modules/renderBaseStationStandard.js
+++ public/modules/renderBaseStationStandard.js
@@ -424,8 +424,8 @@ export function renderBaseStationStandard(app, data = {}, navigate) {
 .ww-morse-pulse{
   position:absolute;
   z-index:24;
-  top:5.3%;
-  left:84.8%;
+  top:5.2%;
+  left:84.7%;
   display:flex;
   align-items:center;
   gap:6px;
