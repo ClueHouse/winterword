076d460c3843519c5abeaa4763daffad34402869
ClueHouse <251077386+ClueHouse@users.noreply.github.com>
2026-05-18 14:29:44 +1200

    Update renderBaseStationStandard.js

--- public/modules/renderBaseStationStandard.js
+++ public/modules/renderBaseStationStandard.js
@@ -286,8 +286,8 @@ export function renderBaseStationStandard(app, data = {}, navigate) {
 }
 
 .ww-solve-hotspot{
-  left:61.8%;
-  top:61.8%;
+  left:21.8%;
+  top:55.8%;
   width:25.4%;
   height:9.2%;
   border-radius:0.75rem;
