95a2870ffbd994fc298b96b5baf673d7932ffde1
ClueHouse <251077386+ClueHouse@users.noreply.github.com>
2026-06-03 08:26:16 +1200

    Update clue-page.js

--- public/modules/clue-page.js
+++ public/modules/clue-page.js
@@ -67,7 +67,7 @@ Yet something’s not at home.
 :root {
   --ww-clue-bg-src: "${clueBackground}";
 
-  --ww-hotspot-group-left: 12%;
+  --ww-hotspot-group-left: 20%;
   --ww-hotspot-group-top: 50.6%;
   --ww-hotspot-gap: 9.2%;
 
