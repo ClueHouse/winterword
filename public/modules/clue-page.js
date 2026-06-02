1726a71b61d45bdb99777bacc4c487d1e7cbe4f9
ClueHouse <251077386+ClueHouse@users.noreply.github.com>
2026-06-02 14:08:31 +1200

    Update clue-page.js

--- public/modules/clue-page.js
+++ public/modules/clue-page.js
@@ -541,7 +541,7 @@ body {
 }
 
 .ww-hotspot-life {
-  left: calc(var(--ww-hotspot-group-left) - 2%);
+  left: calc(var(--ww-hotspot-group-left) - 12%);
   top: calc(var(--ww-hotspot-group-top) + (var(--ww-hotspot-gap) * 2) + 0.65%);
   width: var(--ww-hotspot-life-width);
   height: var(--ww-hotspot-life-height);
