887507615218f0d6388bbf4e16deea5da7e9bd4d
ClueHouse <251077386+ClueHouse@users.noreply.github.com>
2026-06-03 10:00:15 +1200

    Update clue-page.js

--- public/modules/clue-page.js
+++ public/modules/clue-page.js
@@ -75,19 +75,19 @@ Yet something’s not at home.
 
      BASE overlay
   ================================================== */
-  --ww-overlay-base-left: 35%;
-  --ww-overlay-base-top: 60%;
+  --ww-overlay-base-left: 15%;
+  --ww-overlay-base-top: 55%;
   --ww-overlay-base-width: 20%;
   --ww-overlay-base-height: 4.8%;
 
   /* CLUES overlay */
-  --ww-overlay-clues-left: 5%;
+  --ww-overlay-clues-left: 15%;
   --ww-overlay-clues-top: 60%;
   --ww-overlay-clues-width: 30%;
   --ww-overlay-clues-height: 4.8%;
 
   /* LIFE overlay */
-  --ww-overlay-life-left: 5%;
+  --ww-overlay-life-left: 15%;
   --ww-overlay-life-top: 65%;
   --ww-overlay-life-width: 20%;
   --ww-overlay-life-height: 6%;
